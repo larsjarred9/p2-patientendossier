5d7f94262f1aac16401a91440526f2657e1b6a23
Lars Jarred Speetjens <lars@speetjens.net>
2023-02-28 12:45:55 +0100

    setup stage manager

--- src/main/java/com/speetjens/p2patientendossier/HelloApplication.java
+++ src/main/java/com/speetjens/p2patientendossier/HelloApplication.java
@@ -4,19 +4,18 @@ import com.speetjens.p2patientendossier.screens.LoginScreen;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+    public static Stage mainStage;
     public static int[] applicationSize = {1200, 650};
 
     @Override
     public void start(Stage stage) {
 
-        Stage mainStage = stage;
+        mainStage = stage;
         mainStage.setWidth(applicationSize[0]);
         mainStage.setHeight(applicationSize[1]);
         mainStage.setResizable(false);
-        mainStage.setTitle("van de bron - Energy Management ");
+        mainStage.setTitle("ADRZ - Patientdossier");
 
         mainStage.setScene(new LoginScreen().getLoginScreen());
         mainStage.show();
