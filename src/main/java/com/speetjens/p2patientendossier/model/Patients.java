44bc7ad13d4d75864cab98b5e71fa57f7ccdb757
Lars Jarred Speetjens <lars@speetjens.net>
2023-03-03 15:26:29 +0100

    Get patients function

--- src/main/java/com/speetjens/p2patientendossier/model/Patients.java
+++ src/main/java/com/speetjens/p2patientendossier/model/Patients.java
@@ -1,13 +1,31 @@
 package com.speetjens.p2patientendossier.model;
 
+import com.speetjens.p2patientendossier.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Patients {
 
     private final String firstName, lastName, email, phone, birthDate, sex, nationality, address, zip, city, country;
 
-
+    /**
+     * Patient constructor
+     * @param firstName String
+     * @param lastName String
+     * @param email String
+     * @param phone String
+     * @param birthDate String
+     * @param sex String
+     * @param nationality String
+     * @param address String
+     * @param zip String
+     * @param city String
+     * @param country String
+     */
     public Patients(String firstName, String lastName, String email, String phone, String birthDate, String sex, String nationality, String address, String zip, String city, String country) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -23,22 +41,59 @@ public class Patients {
     }
 
     /**
-     * Film constructor
+     * Patient constructor
      * @param result ResultSet
-     * @throws SQLException SQLException
      */
-    public Patients(ResultSet result) throws SQLException {
-        this.firstName = result.getString("firstName");
-        this.lastName = result.getString("lastName");
-        this.email = result.getString("email");
-        this.phone = result.getString("phone");
-        this.birthDate = result.getString("birthDate");
-        this.sex = result.getString("sex");
-        this.nationality = result.getString("nationality");
-        this.address = result.getString("address");
-        this.zip = result.getString("zip");
-        this.city = result.getString("city");
-        this.country = result.getString("country");
+    public Patients(ResultSet result){
+        try {
+            this.firstName = result.getString("first_name");
+            this.lastName = result.getString("last_name");
+            this.email = result.getString("email");
+            this.phone = result.getString("phone");
+            this.birthDate = result.getString("birth_date");
+            this.sex = result.getString("sex");
+            this.nationality = result.getString("nationality");
+            this.address = result.getString("address");
+            this.zip = result.getString("zip");
+            this.city = result.getString("city");
+            this.country = result.getString("country");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Patients() {
+        this.firstName = "";
+        this.lastName = "";
+        this.email = "";
+        this.phone = "";
+        this.birthDate = "";
+        this.sex = "";
+        this.nationality = "";
+        this.address = "";
+        this.zip = "";
+        this.city = "";
+        this.country = "";
+    }
+
+    public ArrayList<Object> getPatients() {
+        try {
+            Connection connection = Database.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM patients");
+            ResultSet result = statement.executeQuery();
+
+            ArrayList<Object> patients = new ArrayList<>();
+
+            while (result.next()) {
+                patients.add(new Patients(result));
+            }
+
+            return patients;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public String getFirstName() {
