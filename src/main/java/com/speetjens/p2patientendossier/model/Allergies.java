3691301c0f34bbdcdb7a966be15d241cec1ef60b
Lars Jarred Speetjens <lars@speetjens.net>
2023-03-03 20:10:57 +0100

    allergies class

---
+++ src/main/java/com/speetjens/p2patientendossier/model/Allergies.java
@@ -0,0 +1,62 @@
+package com.speetjens.p2patientendossier.model;
+
+import com.speetjens.p2patientendossier.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Allergies {
+
+    public final String name;
+
+    /**
+     * Allergies constructor
+     * @param name String
+     */
+
+    public Allergies(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Allergies constructor
+     */
+    public Allergies() {
+        this.name = "";
+    }
+
+    public Allergies(ResultSet result) {
+        try {
+            this.name = result.getString("name");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<Object> getAllergies() {
+        try {
+            Connection connection = Database.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM allergies");
+            ResultSet result = statement.executeQuery();
+
+            ArrayList<Object> allergies = new ArrayList<>();
+
+            while (result.next()) {
+                allergies.add(new Allergies(result));
+            }
+
+            return allergies;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
